3b60dde6b6b1fe8986430b650b2336c372f0c025
David Ohayon <davideytanohayon@gmail.com>
2020-12-23 20:08:20 +0200

    Update make screen not sleep

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from time import sleep
 from sys import argv
 import os
 
+slept = False
 load_dotenv(find_dotenv())
-os.system(f'caffeinate -t {int(argv[1]) * 3600}')
 
 options = Options()
 options.add_argument('--use-fake-ui-for-media-stream')
@@ -43,9 +43,13 @@ servers.find_element(By.CSS_SELECTOR, '[aria-label="  Ｖ１"]').click()
 chats = driver.find_element(By.CSS_SELECTOR, '.sidebar-2K8pFh')
 
 for _ in range(int(argv[1])):
-    sleep(.2)
-    chats.find_element(
-        By.CSS_SELECTOR, '[data-list-item-id="channels___732583805541679191"]').click()
-    sleep(2700)
-    chats.find_element(
-        By.CSS_SELECTOR, '[aria-label="Disconnect"]').click()
+    if (not slept):
+        os.system(f'caffeinate -t {int(argv[1]) * 3600}')
+        slept = True
+    else:
+        sleep(.2)
+        chats.find_element(
+            By.CSS_SELECTOR, '[data-list-item-id="channels___732583805541679191"]').click()
+        sleep(2700)
+        chats.find_element(
+            By.CSS_SELECTOR, '[aria-label="Disconnect"]').click()
